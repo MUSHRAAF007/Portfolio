5b9d6579bfbad247bc552ee55afb8419c66e756a
Mushraaf.S <131686834+MUSHRAAF007@users.noreply.github.com>
2025-04-21 09:05:29 +0530

    Add files via upload

---
+++ portfolio 1/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    VANTA.NET({
+      el: ".vanta-bg",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x892c48,      // You can change this color
+      backgroundColor: 0x000000
+    });
+  });
+  
